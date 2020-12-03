b3cb7243fad5b361142b7499454042214ddf32b6
ZechWolf <zechw@smu.edu>
2020-12-02 16:36:56 -0800

    Cart added

---
+++ frontend/src/models/cartItem.js
@@ -0,0 +1,7 @@
+export default class CartItem {
+    constructor(product, quantity, totalPrice) {
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+}
