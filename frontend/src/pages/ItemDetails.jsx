204dc2f4cd9900681116737397b89086af38bd33
Chelby Rhoades <chelbyjrhoades@gmail.com>
2020-12-04 14:53:14 -0600

    added filter

--- frontend/src/pages/ItemDetails.jsx
+++ frontend/src/pages/ItemDetails.jsx
@@ -36,7 +36,7 @@ function ItemDetails(props) {
                                     onClick={ () =>  cartService.addToCart(item)}>
                                         Add to Cart
                                 </Link>
-                                <Link to={`/distInfo/${item.listingID}`} className="btn btn-secondary btn-lg">More info</Link>
+                                
                             </div>
                         </div>
                     </div>
@@ -45,6 +45,7 @@ function ItemDetails(props) {
         </div>);
     /*
     return (
+        <Link to={`/distInfo/${item.listingID}`} className="btn btn-secondary btn-lg">More info</Link>
     <div className="card"
     {item.name}>
         <div className="card-header">
